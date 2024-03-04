a0d27e7aae0d22d00e44ef8261a4a1e58b29cb75
Radek Tesař <radek.tesar135@gmail.com>
2024-03-04 20:53:09 +0100

    Created User input

--- main.py
+++ main.py
@@ -6,5 +6,11 @@ root.title("Převod měn")
 root.minsize(200,200)
 root.resizable(False, False)
 
+# 1 eur = 25,38
+
+# User input
+amount_input = Entry(width=10, font=("Helvetica", 15))
+amount_input.grid(row=0, column=0)
+
 # Main cycle
 root.mainloop()
