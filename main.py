6534ae9bb0938c081178bb5790aa579ba23fb742
Radek Tesař <radek.tesar135@gmail.com>
2024-03-04 20:57:52 +0100

    Created Label

--- main.py
+++ main.py
@@ -9,8 +9,12 @@ root.resizable(False, False)
 # 1 eur = 25,38
 
 # User input
-amount_input = Entry(width=10, font=("Helvetica", 15))
+amount_input = Entry(width=10, font=("Arial", 15))
 amount_input.grid(row=0, column=0)
+
+#Labels
+czk_label = Label(text="CZK", font=("Arial", 15))
+czk_label.grid(row=0, column=1)
 
 # Main cycle
 root.mainloop()
